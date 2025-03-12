f973b0a558ac81c93607091e518a1893ec0d9240
marty957 <fantuzzimartina@yahoo.it>
2025-03-12 22:03:45 +0100

    aggiunto pulsante in position absolute

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -49,8 +49,14 @@ function Profile() {
       <Container className="my-4">
         <div className="row">
           <div className="col-sm-12 col-md-5 col-xl-3">
-            <div className="my-2 ">
-              <img src="../src/assets/profilo.webp" width={"200px"} className="rounded-circle" />
+            <div className="my-2 position-relative container">
+              <img src="../src/assets/profilo.webp" className=" rounded-circle  img-fluid" />
+              <div className=" position-absolute add link">
+                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentcolor" className="bi bi-plus-circle" viewBox="0 0 16 16">
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
+                </svg>
+              </div>
             </div>
           </div>
           <div className="col-sm-12 col-md-7 col-xl-8">
