924bbd872a7b5483a884abf0401b32b332282695
marty957 <fantuzzimartina@yahoo.it>
2025-03-16 17:26:50 +0100

    iniziata fetch per aggiunger certificati con l'upload del pdf. manca la gestione dei loadin, prop on hide e il cathc

--- src/components/CertifcateSection.jsx
+++ src/components/CertifcateSection.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Card, Col, Container, Row, Spinner } from "react-bootstrap";
 import { useParams } from "react-router-dom";
+import ModalAddCertificate from "./ModalAddCertificate";
 
 function CertficateSection() {
   const token = localStorage.getItem("token");
@@ -9,7 +10,10 @@ function CertficateSection() {
   const [certificates, setCertificates] = useState([]);
   const [loading, setLoading] = useState();
 
+  const [modalShow, setModalShow] = useState();
+
   const uploadinfCertificate = async () => {
+    setLoading(true);
     try {
       const response = await fetch(`http://localhost:8080/api/certificate/${id}/all`, {
         headers: {
@@ -18,11 +22,14 @@ function CertficateSection() {
       });
 
       if (response.status === 200) {
+        setLoading(false);
         const data = await response.json();
         setCertificates(data);
         console.log(data);
       }
-    } catch {}
+    } catch (err) {
+      console.log("Errore nella fetch", err);
+    }
   };
 
   useEffect(() => {
@@ -41,12 +48,13 @@ function CertficateSection() {
       <Container>
         <div className="d-flex  justify-content-center mb-3">
           <h3 className="pt-2 me-2">I tuoi certificati</h3>
-          <button type="submit" style={{ cursor: "pointer", backgroundColor: "transparent", border: "none" }}>
+          <button type="button" style={{ cursor: "pointer", backgroundColor: "transparent", border: "none" }} onClick={() => setModalShow(true)}>
             <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="#22a7e0" className="bi bi-plus-circle" viewBox="0 0 16 16">
               <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
               <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
             </svg>
           </button>
+          <ModalAddCertificate show={modalShow} onHide={() => setModalShow(false)} />
         </div>
         {certificates.length === 0 ? (
           <div className="container">
---
+++ src/components/ModalAddCertificate.jsx
@@ -0,0 +1,109 @@
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import { useRef, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function ModalAddCertificate(props) {
+  const [certificate, setCertificate] = useState({ name: "", description: "", file: null, issueDate: "", expireDate: "", category: "" });
+  const { id } = useParams();
+  const token = localStorage.getItem("token");
+  console.log(token);
+  console.log(id);
+  const fileUploader = useRef();
+
+  const addCetificate = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("name", certificate.name);
+    formData.append("description", certificate.description);
+    formData.append("issueDate", certificate.issueDate);
+    formData.append("expireDate", certificate.expireDate);
+    // If you have a file to upload, append it here
+    if (certificate.file) {
+      formData.append("file", certificate.file); // Assuming 'certificate.file' holds the file
+    }
+    fetch(`http://localhost:8080/api/certificate/${id}/new`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      body: formData
+    }).then((resp) => {
+      console.log(resp);
+      return resp.text();
+    });
+  };
+
+  return (
+    <Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">Nuovo Certificato</Modal.Title>
+      </Modal.Header>
+      <Form onSubmit={addCetificate}>
+        <Modal.Body>
+          <Form.Group className="mb-3" controlId="course">
+            <Form.Label>Course's name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Course's name"
+              value={certificate.name}
+              onChange={(e) => setCertificate({ ...certificate, name: e.target.value })}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="description">
+            <Form.Label>Short description of the course</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={2}
+              value={certificate.description}
+              onChange={(e) => setCertificate({ ...certificate, description: e.target.value })}
+            />
+          </Form.Group>
+          <Form.Group controlId="formFile" className="mb-3">
+            <Form.Label>Certificat PDF </Form.Label>
+            <Form.Control type="file" accept="application/pdf" onChange={(e) => setCertificate({ ...certificate, file: e.target.files[0] })} />
+          </Form.Group>
+          <Form.Group controlId="category" className="mb-3">
+            <Form.Label>Type of Certificate </Form.Label>
+            <Form.Select
+              aria-label="Default select example"
+              value={certificate.category || "GENERAL"}
+              onChange={(e) => setCertificate({ ...certificate, category: e.target.value })}
+            >
+              <option></option>
+              <option value="GENERAL">GENERAL</option>
+              <option value="MEDIICAL">MEDICAL</option>
+              <option value="SECURITY">SECURITY</option>
+              <option value="VISA">VISA</option>
+            </Form.Select>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="course">
+            <Form.Label>Issue Date</Form.Label>
+            <Form.Control
+              type="date"
+              placeholder="Course's name"
+              value={certificate.issueDate}
+              onChange={(e) => setCertificate({ ...certificate, issueDate: e.target.value })}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="course">
+            <Form.Label>Expire Date</Form.Label>
+            <Form.Control
+              type="date"
+              placeholder="Course's name"
+              value={certificate.expireDate}
+              onChange={(e) => setCertificate({ ...certificate, expireDate: e.target.value })}
+            />
+          </Form.Group>
+        </Modal.Body>
+        <Modal.Footer>
+          <button type="submit" className="button py-1 " style={{ color: "#ffffff", borderRadius: "8px", cursor: "pointer" }}>
+            Add Certificate
+          </button>
+        </Modal.Footer>
+      </Form>
+    </Modal>
+  );
+}
+export default ModalAddCertificate;
